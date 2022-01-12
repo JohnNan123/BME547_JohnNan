f9cb13ed7c04594ebcf3542c15bb958721455ce6
Junyu Nan <junyunan@Junyus-MacBook-Pro.local>
2022-01-12 16:30:17 -0500

    added in check HDL level function

--- blood_calculator.py
+++ blood_calculator.py
@@ -9,4 +9,19 @@ def interface():
             keep_running = False
     return 
 
+def accept_input(test_name): 
+    entry = input("Enter the test result here: ")
+    return int(entry)
+
+
+def check_HDL(HDL_value): 
+    if HDL_value >= 60: 
+        answer = 'Normal'
+    elif HDL_value >= 40 && < 60: 
+        answer = 'Borderline Low'
+    else: 
+        answer = 'Low'
+    return answer
+
 interface()
+
