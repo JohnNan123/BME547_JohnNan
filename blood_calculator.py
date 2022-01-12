77d8917ddd6ce2c18a1ae0a7c6728ba26e76e201
Junyu Nan <junyunan@Junyus-MacBook-Pro.local>
2022-01-12 16:37:12 -0500

    added HDL driver and print result function

--- blood_calculator.py
+++ blood_calculator.py
@@ -10,8 +10,12 @@ def interface():
     return 
 
 def accept_input(test_name): 
-    entry = input("Enter the test result here: ")
+    entry = input("Enter the {} test result here: ".format(test_name))
     return int(entry)
+
+def print_result(test_name, test_vaule, test_class): 
+    out_string = "The test value of {} for {} is {}.".format(test_vaule, test_name, test_class)
+    print(out_string)
 
 
 def check_HDL(HDL_value): 
@@ -22,6 +26,15 @@ def check_HDL(HDL_value):
     else: 
         answer = 'Low'
     return answer
+
+def HDL_driver():
+    HDL_value = accept_input("HDL")
+    classification = check_HDL(HDL_value)
+    print_result("HDL", HDL_value, classification)
+
+    
+
+
 
 interface()
 
