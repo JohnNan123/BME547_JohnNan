088f6e864587c29dd92adbded1559afcb16ca43b
Junyu Nan <junyunan@Junyus-MacBook-Pro.local>
2022-02-02 16:21:06 -0500

    add those files with .github

--- test_weight_entry.py
+++ test_weight_entry.py
@@ -10,6 +10,11 @@ import pytest
 @pytest.mark.parametrize("input, expected", [
     ("22 lb", 10),
     ("50 kg", 50),
+    ("23.6kg", 24),
+    ("22 lbs", 10),
+    ("22lbs", 10),
+    ("22 LB", 10),
+    ("-22 lb", -10)
     ])
 def test_parse_weight_input(input, expected):
     from weight_entry import parse_weight_input
